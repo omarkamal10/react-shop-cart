4fd5fb2e4891b5d99168de44e8f351f937cb9156
unknown <omroomessi@gmail.com>
2021-07-30 20:19:03 +0200

    final final

--- server.js
+++ server.js
@@ -96,5 +96,5 @@ app.delete("/api/orders/:id", async (req, res) => {
   res.send(order);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("serve at http://localhost:5000"));
